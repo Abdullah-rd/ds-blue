286748481fff6bb65222577f2b5a2c1d4989cd7e
Abdullah <139013835+Abdullah-rd@users.noreply.github.com>
2023-08-26 15:17:37 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,54 +1,53 @@
-let isDarkMode = false;
+const bodyElement = document.querySelector('body');
+const elementsToToggle = [
+  'title',
+  'lightmode',
+  'default3bar',
+  'content',
+  'horline',
+  'content2',
+  'writer',
+  'hbtn',
+  'skill-title',
+  'percentage',
+  'skills-title',
+  'article-title',
+  'articles-title',
+  'footer'
+];
+
+let isDarkMode = JSON.parse(localStorage.getItem('dark')) || false;
+if (isDarkMode) setDark();
+
 
 function toggleDarkMode() {
-  const bodyElement = document.querySelector('body');
-  const elementsToToggle = [
-    'pbtn3',
-    'title',
-    'lightmode',
-    'default3bar',
-    'content',
-    'horline',
-    'content2',
-    'writer',
-    'hbtn',
-    'skill-title',
-    'percentage',
-    'skills-title',
-    'article-title',
-    'articles-title',
-    'footer'
-  ];
-
-  if (isDarkMode) {
-    document.querySelector('.Github').innerHTML = '  <img src="images/github.png" alt="" class="skill-logo">     Github';
-
-    bodyElement.classList.remove('body-darkmode');
-    elementsToToggle.forEach(className => {
-      const elements = document.querySelectorAll('.' + className);
-      elements.forEach(element => {
-        element.classList.remove(className + '-darkmode');
-        element.style.color = ''; 
-      });
-    });
-
-  } else {
-    document.querySelector('.Github').innerHTML = '<img src="images/Github-w.png" alt="" class="skill-logo">     Github';
-    bodyElement.classList.add('body-darkmode');
-    elementsToToggle.forEach(className => {
-      const elements = document.querySelectorAll('.' + className);
-      elements.forEach(element => {
-        element.classList.add(className + '-darkmode');
-        if (className !== 'hbtn') {
-            element.style.color = 'white'; 
-          }      
-        });
-    });
-
-  }
-
+  isDarkMode ? setLight() : setDark();
   isDarkMode = !isDarkMode;
+  localStorage.setItem('dark', JSON.stringify(isDarkMode));
 }
 
+function setDark() {
+  document.querySelector('.Github').innerHTML = '<img src="images/Github-w.png" alt="" class="skill-logo">     Github';
+  bodyElement.classList.add('body-darkmode');
+  elementsToToggle.forEach(className => {
+    const elements = document.querySelectorAll('.' + className);
+    elements.forEach(element => {
+      element.classList.add(className + '-darkmode');
+      if (className !== 'hbtn' && className !== 'content' && className !== 'content2' && className !== 'writer') {
+        element.style.color = 'white'; 
+      }      
+    });
+  });
+}
 
-
+function setLight() {
+  document.querySelector('.Github').innerHTML = '  <img src="images/github.png" alt="" class="skill-logo">     Github';
+  bodyElement.classList.remove('body-darkmode');
+  elementsToToggle.forEach(className => {
+    const elements = document.querySelectorAll('.' + className);
+    elements.forEach(element => {
+      element.classList.remove(className + '-darkmode');
+      element.style.color = ''; 
+    }); 
+  });
+}
